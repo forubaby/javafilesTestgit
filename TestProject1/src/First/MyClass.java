e05bc12869721fa3ec165eb4d01613097f96407c
Sai Charan-DELL <Sai-Charan-DELL@DESKTOP-PMIFEDC>
2022-01-24 11:02:02 +0530

    2nd line removed

--- TestProject1/src/First/MyClass.java
+++ TestProject1/src/First/MyClass.java
@@ -4,7 +4,7 @@ public class MyClass
 {
 	public static void main(String[] args) {
 		System.out.println("first project to practise git");
-		System.out.println("adding new line ");
+		
 		System.out.println("new line2");
 	}
 
